34692df52aca156ab525e083ed0ca5d6785dad0a
Robel Tirfe <robel@gmail.com>
2018-11-06 08:53:34 -0500

    Adding an app.js file

---
+++ app.js
@@ -0,0 +1,39 @@
+const request = require("request");
+const fs = require("fs");
+
+let name = "KN4EZY-1"
+let apiKey = "117986.5w5ElPStqtCEEfG"
+let url = `https://api.aprs.fi/api/get?name=${name}&what=loc&apikey=${apiKey}&format=json`;
+
+const REQUESTRATE = 60 * 1000;  //How often to request APRS data in ms
+
+let dataObj = {
+	    table: []
+}
+
+console.log(`Pulling location data for ${name} every ${REQUESTRATE / 1000} seconds.\n`);
+
+RequestData();
+setInterval(RequestData, REQUESTRATE);
+
+function RequestData(){
+	    console.log(`Getting APRS data for ${name}...`)
+	    request(url, (error, response, body) =>{
+		            if(error){
+				                console.log("Could not pull data, retrying...");
+				                RequestData();
+				            }
+		            console.log("Writing location data to file");
+		            WriteData(body);
+		        })
+}
+
+function WriteData(data){
+	    dataObj.table.push(data);
+	    let json = JSON.stringify(dataObj);
+	    fs.writeFile('locationData.json', json, 'utf8', (err)=>{
+		            if(err){
+				                console.log(err);
+				            }
+		        })
+}
