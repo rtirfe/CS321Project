91e36904c95583689a84cb029fe99890891cc490
Robel Tirfe <robel@gmail.com>
2018-11-06 09:07:49 -0500

    Setting more env varialbes to run the app on heroku

--- app.js
+++ app.js
@@ -43,6 +43,22 @@ function WriteData(data){
 }
 
 
+app.get("/" (req, res) => {
+	res.send("This is root for CS321 Project.\n");
+	res.end();
+});
+
+app.get("*", (req,res, next) =>{
+	var err = new Error();
+	err.status = 500;
+	next(err);
+});
+app.use( (err, req, res, next) =>{
+	if(err,status != 500){
+		return next();
+	}
+	res.send( err.message || " ** Invalide request ** \n");
+});
 app.listen(app.get('port'), ()=> {
 	console.log("Node app.js is running on port" + app.get('port'));
 });
